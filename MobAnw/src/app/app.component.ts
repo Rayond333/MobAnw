0910af26fdc6bd097ab54ae6645d92606bd6496d
Raimund <48927431+Rayond333@users.noreply.github.com>
2020-11-19 19:35:01 +0100

    Webcam module hinzugefügt

--- MobAnw/src/app/app.component.ts
+++ MobAnw/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { WebcamImage } from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MobAnw';
+
+export class AppComponent { 
+  title = 'gfgangularwebcam'; 
+  
+  public webcamImage: WebcamImage = null; 
+  private trigger: Subject<void> = new Subject<void>(); 
+  triggerSnapshot(): void { 
+   this.trigger.next(); 
+  } 
+  handleImage(webcamImage: WebcamImage): void { 
+   console.info('Saved webcam image', webcamImage); 
+   this.webcamImage = webcamImage; 
+  } 
+   
+  public get triggerObservable(): Observable<void> { 
+   return this.trigger.asObservable(); 
+  } 
 }
--- MobAnw/src/app/app.module.ts
+++ MobAnw/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { WebcamModule } from 'ngx-webcam';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    WebcamModule,
+    FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
