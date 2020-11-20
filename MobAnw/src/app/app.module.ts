f2f0943956dd2c4ebaf8e5ca8017d94824aaee64
Raimund <48927431+Rayond333@users.noreply.github.com>
2020-11-20 14:24:19 +0100

    Added components "map" & "webcam"

--- MobAnw/src/app/app.module.ts
+++ MobAnw/src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { WebcamModule } from 'ngx-webcam';
 import { FormsModule } from '@angular/forms';
+import { MapComponent } from './map/map.component';
+import { WebcamComponent } from './webcam/webcam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    WebcamComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'map', component: MapComponent},
+      {path: 'webcam', component: WebcamComponent}
+    ]),
     WebcamModule,
     FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
